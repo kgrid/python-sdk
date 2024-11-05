ba680c9b4c94670fcde66d33a4e397077afebbf8
Farid Seifi <farid.seifi@gmail.com>
2024-11-05 09:36:59 -0500

    fixed fixed metadata access from the package

--- ko/ko.py
+++ ko/ko.py
@@ -1,18 +1,29 @@
 import json
 import os
+# import pkg_resources
+import importlib.resources as resources
 
 class Ko:
-    def __init__(self, metadata_file='metadata.json'):
+    METADATA_FILE = "metadata.json" # by default it is located in the root of the ko
+    def __init__(self,package_name, metadata_file=METADATA_FILE):
+        self.package_name = package_name
         self.metadata_file = metadata_file
         self.metadata = self._load_metadata()
        
-    def _load_metadata(self):
-        if os.path.exists(self.metadata_file):
-            with open(self.metadata_file, 'r') as file:
-                return json.load(file)
-        else:
-            raise FileNotFoundError(f"{self.metadata_file} not found")
-
+    def _load_metadata(self):     
+       
+        try:
+            package_root = resources.files(self.package_name)
+            metadata_path = package_root.parent / self.metadata_file
+            print("-> " + str(metadata_path))
+            if metadata_path.exists():
+                with open(metadata_path, 'r') as file:
+                    return json.load(file)
+            else:
+                raise FileNotFoundError(f"{metadata_path} not found")
+        except ModuleNotFoundError:
+            raise ModuleNotFoundError(f"Package '{self.package_name}' not found")
+        
     def get_version(self):
         return self.metadata.get('version', 'Unknown version')
     
